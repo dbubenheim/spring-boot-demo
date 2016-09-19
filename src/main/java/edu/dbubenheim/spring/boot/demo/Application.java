2d2e80d9c8c923b2aa16487601fc29ac6c8a3c49
Daniel Bubenheim <daniel.bubenheim@gmail.com>
2016-09-19 11:27:38 +0200

    Initial commit

---
+++ src/main/java/edu/dbubenheim/spring/boot/demo/Application.java
@@ -0,0 +1,15 @@
+package edu.dbubenheim.spring.boot.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author dbubenheim@communicode.de
+ */
+@SpringBootApplication
+public class Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+}
